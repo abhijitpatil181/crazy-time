c8762fb6c415e217b6fbb594a98a9de5b2af34e4
abhijitpatil181 <patilabhijit181@gmail.com>
2025-05-18 19:32:57 +0530

    local ip added for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { queryClient } from "./apis/services";
 const { VITE_SERVER_BASE_IP } = import.meta.env;
 axios.defaults.baseURL = VITE_SERVER_BASE_IP;
 console.log("VITE_SERVER_BASE_IP", VITE_SERVER_BASE_IP);
+console.log("vite is local", import.meta.env.VITE_IS_LOCAL);
 
 function App() {
   return (
